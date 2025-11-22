264b52d22538440b2abb34d10105c834b33c609b
evanovar <evanyzited@gmail.com>
2025-11-22 09:38:52 +0700

    update version

--- utils/ui.py
+++ utils/ui.py
@@ -17,7 +17,7 @@ class AccountManagerUI:
     def __init__(self, root, manager):
         self.root = root
         self.manager = manager
-        self.APP_VERSION = "2.2.6"
+        self.APP_VERSION = "2.2.7"
         self._game_name_after_id = None
         
         try:
