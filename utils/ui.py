6c24de4232855cdfe48b23691f7fdffad79a2f3a
evanovar <evanyzited@gmail.com>
2025-12-30 08:21:07 +0700

    update version

--- utils/ui.py
+++ utils/ui.py
@@ -23,7 +23,7 @@ class AccountManagerUI:
     def __init__(self, root, manager):
         self.root = root
         self.manager = manager
-        self.APP_VERSION = "2.3.5"
+        self.APP_VERSION = "2.3.6"
         self._game_name_after_id = None
         self._save_settings_timer = None
         
