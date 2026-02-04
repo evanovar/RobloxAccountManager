837cc6bdbf1a32c9fb1e600cf6e0f2675482276b
evanovar <evanyzited@gmail.com>
2026-02-04 18:22:30 +0700

    update version

--- utils/ui.py
+++ utils/ui.py
@@ -38,7 +38,7 @@ class AccountManagerUI:
         self.root = root
         self.manager = manager
         self.icon_path = icon_path
-        self.APP_VERSION = "2.4.3"
+        self.APP_VERSION = "2.4.4"
         self._game_name_after_id = None
         self._save_settings_timer = None
         
