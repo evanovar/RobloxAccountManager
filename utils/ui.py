c092caef83a8e52fe55d550b25ad263049a983c0
evanovar <evanyzited@gmail.com>
2026-04-03 20:20:07 +0700

    update version

--- utils/ui.py
+++ utils/ui.py
@@ -52,7 +52,7 @@ class AccountManagerUI:
         self.icon_path = icon_path
         self.discord_logo_path = discord_logo_path
         self.discord_logo_img = None
-        self.APP_VERSION = "2.4.6"
+        self.APP_VERSION = "2.4.7"
         self._game_name_after_id = None
         self._save_settings_timer = None
         
